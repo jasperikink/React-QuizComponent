8d30f224c9bea723d286b9e458e2c232c6a61767
Jasper Ikink <ikinkjasper@gmail.com>
2020-11-01 14:26:48 +0100

    hier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
+import Quiz from './Quiz.js'
 import './App.css'
-import Quiz from './quiz.js'
 
 class App extends Component {
   render() {
     return (
-
       <Quiz />
     )
   }
--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,3 @@
-import { render } from 'enzyme'
 import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
